138dfa4ad3bb961822deb6c31eeaa24db3704149
dachaoxiaoran <dachaoxiaoran@localhost>
2016-07-19 20:57:39 +0800

    dao提交

--- src/com/mum/db/DAO.java
+++ src/com/mum/db/DAO.java
@@ -17,27 +17,46 @@ import com.mysql.jdbc.Statement;
  * 数据库操作
  * @author 王超
  */
-public class DbOperator {
+public class DAO {
+	
+	/**
+	 * 类静态对象，唯一
+	 */
+	private static DAO dao;
 	
 	/**
 	 * @see com.mum.db.init.Conn
 	 */
-	private Conn conn;
+	private static Conn conn;
 	
 	/**
 	 * @see com.mum.db.SqlBuild
 	 */
-	private SqlBuild sqlBuild;
+	private static SqlBuild sqlBuild;
+	
+	/**
+	 * {@link com.mum.db.pojo.DbTable}列表
+	 */
+	private static List<DbTable> dbTables;
 	
 	/**
 	 * 初始化{@link #conn}和{@link #sqlBuild}
-	 * @throws ClassNotFoundException
-	 * @throws SQLException
 	 */
-	public DbOperator() throws ClassNotFoundException, SQLException {
-		conn = new Conn();
+	static {
+		try {
+			conn = new Conn();
+		} catch (ClassNotFoundException | SQLException e) {
+			throw new ExceptionInInitializerError(e);
+		}
+		dao = new DAO();
 		sqlBuild = new SqlBuild();
+		dbTables = new ArrayList<>();
 	}
+	
+	/**
+	 * 不允许实例化
+	 */
+	private DAO() {}
 
 	/**
 	 * 数据库增删改操作
@@ -45,11 +64,13 @@ public class DbOperator {
 	 * @return 增删改操作的行数
 	 * @throws SQLException
 	 */
-	public int[] modify(DbTable... dbTables) throws SQLException {
+	public static int[] modify(DbTable... dbTables) throws SQLException {
 		try(Statement statement = (Statement) conn.getConnection().createStatement()) {
+			for (DbTable dbTable : DAO.dbTables) statement.addBatch(sqlBuild(dbTable));
 			for (DbTable dbTable : dbTables) statement.addBatch(sqlBuild(dbTable));
 			int[] res = statement.executeBatch();
 			statement.getConnection().commit();
+			DAO.dbTables.clear();
 			return res;
 		}
 	}
@@ -60,7 +81,7 @@ public class DbOperator {
 	 * @return 查询得到的数据
 	 * @throws SQLException
 	 */
-	public List<Map<String, String>> select(DbTable dbTable) throws SQLException {
+	public static List<Map<String, String>> select(DbTable dbTable) throws SQLException {
 		List<Map<String, String>> selectList = new ArrayList<>();
 		try(Statement statement = (Statement) conn.getConnection().createStatement();
 			ResultSet resultSet = statement.executeQuery(sqlBuild.select(dbTable).toString())) {
@@ -75,11 +96,21 @@ public class DbOperator {
 	}
 	
 	/**
+	 * 为{@link #dbTables}添加元素
+	 * @param dbTables {@link com.mum.db.pojo.DbTable}数组
+	 * @return this{@link com.mum.db.DAO}的唯一静态对象
+	 */
+	public static DAO addTables(DbTable... dbTables) {
+		for (DbTable dbTable : dbTables) DAO.dbTables.add(dbTable);
+		return dao;
+	}
+	
+	/**
 	 * 生成sql语句
 	 * @param dbTable {@link com.mum.db.pojo.DbTable}
 	 * @return sql语句
 	 */
-	private String sqlBuild(DbTable dbTable) {
+	private static String sqlBuild(DbTable dbTable) {
 		if (dbTable.getDbOperateEnum() == DbOperateEnum.INSERT) return sqlBuild.insert(dbTable).toString();			//插入
 		else if (dbTable.getDbOperateEnum() == DbOperateEnum.UPDATE) return sqlBuild.update(dbTable).toString();	//更新
 		else if (dbTable.getDbOperateEnum() == DbOperateEnum.DELETE) return sqlBuild.delete(dbTable).toString();	//删除
